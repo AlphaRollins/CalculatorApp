aff9c3522d1150602ec8368c9874fe8371d57f51
JuanA <juan.alegria2008@gmail.com>
2025-04-04 09:13:17 -0500

    cambios

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
-const display = document.querySelector('#display')
+const display = document.querySelector("#display");
 
-function appendToDisplay(input){
-    display.value+=input
+function appendToDisplay(input) {
+  display.value += input;
 }
 
-function clearDisplay(){
-    display.value= ""
+function clearDisplay() {
+  display.value = "";
+}
+
+function suma(num1, num2) {
+  return num1 + num2;
+}
+function resta(num1, num2) {
+  return num1 - num2;
+}
+function multiplicaion(num1, num2) {
+  return num1 * num2;
+}
+function division(num1, num2) {
+  return num1 / num2;
 }
 
 function calculate(){
